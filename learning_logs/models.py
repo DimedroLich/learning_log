500f8a22e8e48f28e619a9ad72d55b3f77b65fce
DimedroLich <1DimedroL1@gmail.com>
2023-03-30 15:32:39 +0300

    Added one more model - Entry. Both models added to admin panel. Added some info to both db tables via admin panel

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Topic(models.Model):
     """Тема, которую изучает пользователь"""
@@ -8,3 +9,18 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Entry(models.Model):
+    """Информация изученная пользователем по теме"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        return f"{self.text[:50]}..."
