aeae44530db8ea0f5aa3cdae86515fb26f0a2b09
DimedroLich <1DimedroL1@gmail.com>
2023-04-04 12:05:12 +0300

    Added page and template 'topics'. With listing all topics from db

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Topic, Entry
+
 
 # Create your views here.
 
 def index(request):
-    return render(request,'learning_logs/index.html')
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    context = {
+        'topics' : Topic.objects.order_by('date_added'),
+    }
+    return render(request, 'learning_logs/topics.html', context=context)
